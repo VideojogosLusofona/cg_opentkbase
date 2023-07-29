0b913e4b61ac98ff51b0954adc45b3703bd4b44f
Diogo Andrade <diogo.andrade@mind.pt>
2023-07-29 13:15:48 +0100

    Various exercises from class 2

--- main.cs
+++ main.cs
@@ -11,7 +11,7 @@ namespace SDLBase
 
             app.Initialize();
 
-            ExecuteApp_Clear(app);
+            ExecuteApp_TrianglesImmediateColorPerVertexBlend(app);
 
             app.Shutdown();
         }
@@ -24,6 +24,112 @@ namespace SDLBase
                 GL.Clear(ClearBufferMask.ColorBufferBit);
             });
 
+        }
+
+        static void ExecuteApp_TriangleImmediate(OpenTKApp app)
+        {
+            app.Run(() =>
+            {
+                GL.ClearColor(0.2f, 0.4f, 0.2f, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+                
+                GL.Begin(PrimitiveType.Triangles);
+                    GL.Vertex3( 0.0f,  0.5f, 0.0f);
+                    GL.Vertex3( 0.5f, -0.5f, 0.0f);
+                    GL.Vertex3(-0.5f, -0.5f, 0.0f);
+                GL.End();
+            });
+        }
+
+        static void ExecuteApp_TriangleImmediateColor(OpenTKApp app)
+        {
+            app.Run(() =>
+            {
+                GL.ClearColor(0.2f, 0.4f, 0.2f, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                GL.Color4(1.0f, 1.0f, 0.0f, 1.0f);
+
+                GL.Begin(PrimitiveType.Triangles);
+                GL.Vertex3(0.0f, 0.5f, 0.0f);
+                GL.Vertex3(0.5f, -0.5f, 0.0f);
+                GL.Vertex3(-0.5f, -0.5f, 0.0f);
+                GL.End();
+            });
+        }
+
+
+        static void ExecuteApp_TriangleImmediateColorPerVertex(OpenTKApp app)
+        {
+            app.Run(() =>
+            {
+                GL.ClearColor(0.2f, 0.4f, 0.2f, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                GL.Begin(PrimitiveType.Triangles);
+                GL.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.0f, 0.5f, 0.0f);
+                GL.Color4(1.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.5f, -0.5f, 0.0f);
+                GL.Color4(0.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(-0.5f, -0.5f, 0.0f);
+                GL.End();
+            });
+        }
+
+        static void ExecuteApp_TrianglesImmediateColorPerVertex(OpenTKApp app)
+        {
+            app.Run(() =>
+            {
+                GL.ClearColor(0.2f, 0.4f, 0.2f, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                GL.Begin(PrimitiveType.Triangles);
+
+                GL.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.0f, 0.5f, 0.0f);
+                GL.Color4(1.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.5f, -0.5f, 0.0f);
+                GL.Color4(0.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(-0.5f, -0.5f, 0.0f);
+
+                GL.Color4(0.0f, 0.5f, 1.0f, 1.0f);
+                GL.Vertex3(0.2f, 0.6f, 0.0f);
+                GL.Color4(0.8f, 0.4f, 1.0f, 1.0f);
+                GL.Vertex3(0.7f, -0.4f, 0.0f);
+                GL.Color4(0.0f, 1.0f, 1.0f, 1.0f);
+                GL.Vertex3(-0.3f, -0.4f, 0.0f);
+
+                GL.End();
+            });
+        }
+
+        static void ExecuteApp_TrianglesImmediateColorPerVertexBlend(OpenTKApp app)
+        {
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            app.Run(() =>
+            {
+                GL.ClearColor(0.2f, 0.4f, 0.2f, 1.0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+                GL.Begin(PrimitiveType.Triangles);
+
+                GL.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.0f, 0.5f, 0.0f);
+                GL.Color4(1.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(0.5f, -0.5f, 0.0f);
+                GL.Color4(0.0f, 1.0f, 0.0f, 1.0f);
+                GL.Vertex3(-0.5f, -0.5f, 0.0f);
+
+                GL.Color4(0.0f, 0.0f, 0.0f, 0.5f);
+                GL.Vertex3(0.2f, 0.6f, 0.0f);
+                GL.Vertex3(0.7f, -0.4f, 0.0f);
+                GL.Vertex3(-0.3f, -0.4f, 0.0f);
+
+                GL.End();
+            });
         }
     }
 }
