108a66fbbf54d97bf8dc7a1c259ead6f16d65afb
Diogo Andrade <diogo.andrade@mind.pt>
2023-08-12 17:35:05 +0100

    Added tangent in mesh, and methods for constructing tangent space

--- Mesh.cs
+++ Mesh.cs
@@ -11,6 +11,7 @@ namespace OpenTKBase
         private PrimitiveType   primitive = PrimitiveType.Triangles;
         private List<Vector3>   vertices;
         private List<Vector3>   normals;
+        private List<Vector4>   tangents;
         private List<Color4>    colors;
         private List<Vector2>   uvs;
         private List<uint>      indices;
@@ -26,6 +27,7 @@ namespace OpenTKBase
         {
             public Vector3  position;
             public Vector3  normal;
+            public Vector4  tangent;
             public Color4   color;
             public Vector2  uv;
 
@@ -64,6 +66,14 @@ namespace OpenTKBase
         }
 
         public List<Vector3> GetNormals() => normals;
+        
+        public void SetTangents(List<Vector4> tangents)
+        {
+            this.tangents = tangents;
+            vertexDirty = true;
+        }
+
+        public List<Vector4> GetTangents() => tangents;
 
         public void SetColors(List<Color4> colors)
         {
@@ -80,6 +90,100 @@ namespace OpenTKBase
         }
 
         public List<Vector2> GetUVs() => uvs;
+
+        public void ComputeNormalsAndTangentSpace()
+        {
+            var indices = this.indices;
+            var vertices = this.vertices;
+            var uv = this.uvs;
+
+            List<Vector3> triangle_normals = new List<Vector3>();
+            List<Vector3> triangle_tangents = new List<Vector3>();
+            List<Vector3> triangle_binormals = new List<Vector3>();
+            List<float> triangle_areas = new List<float>();
+            List<List<int>> triangles_per_vertex = new List<List<int>>();
+            for (int i = 0; i < vertices.Count; i++) triangles_per_vertex.Add(new List<int>());
+
+            for (int i = 0; i < indices.Count; i += 3)
+            {
+                uint i0, i1, i2;
+                i0 = indices[i];
+                i1 = indices[i + 1];
+                i2 = indices[i + 2];
+
+                Vector3 v0, v1, v2;
+                Vector2 uv0, uv1, uv2;
+                v0 = vertices[(int)i0]; uv0 = uv[(int)i0];
+                v1 = vertices[(int)i1]; uv1 = uv[(int)i1];
+                v2 = vertices[(int)i2]; uv2 = uv[(int)i2];
+
+                var side0 = v0 - v1;
+                var side1 = v2 - v1;
+
+                // Normal and triangle area
+                Vector3 normal = Vector3.Cross(side1, side0);
+                triangle_areas.Add(normal.Length * 0.5f);
+                normal = normal.Normalized();
+                triangle_normals.Add(normal);
+
+                // Tangent space
+                Vector2 delta0 = uv0 - uv1;
+                Vector2 delta1 = uv2 - uv1;
+
+                Vector3 tangent = (delta1.Y * side0 - delta0.Y * side1).Normalized();
+                Vector3 binormal = (delta1.X * side0 - delta0.X * side1).Normalized();
+
+                var tangent_cross = Vector3.Cross(tangent, binormal);
+                if (Vector3.Dot(tangent_cross, normal) < 0.0f)
+                {
+                    tangent = -tangent;
+                    binormal = -binormal;
+                }
+
+                triangle_binormals.Add(binormal);
+                triangle_tangents.Add(tangent);
+
+                triangles_per_vertex[(int)i0].Add((int)i / 3);
+                triangles_per_vertex[(int)i1].Add((int)i / 3);
+                triangles_per_vertex[(int)i2].Add((int)i / 3);
+            }
+
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector4> tangents = new List<Vector4>();
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                normals.Add(Vector3.Zero);
+                tangents.Add(Vector4.Zero);
+            }
+
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                Vector3 normal = Vector3.Zero;
+                Vector3 tangent = Vector3.Zero;
+                Vector3 binormal = Vector3.Zero;
+                float sign = 1.0f;
+
+                foreach (var index in triangles_per_vertex[i])
+                {
+                    normal += triangle_normals[index];
+                    tangent += triangle_tangents[index];
+                    binormal += triangle_binormals[index];
+                }
+
+                normal.Normalize();
+                tangent.Normalize();
+                binormal.Normalize();
+
+                var tmp = Vector3.Cross(normal, tangent);
+                if (Vector3.Dot(tmp, binormal) < 0) sign = -1.0f; else sign = 1.0f;
+
+                normals[i] = normal;
+                tangents[i] = new Vector4(tangent, sign);
+            }
+
+            SetNormals(normals);
+            SetTangents(tangents);
+        }
 
         public void RenderImmediate()
         {
@@ -157,6 +261,7 @@ namespace OpenTKBase
             {
                 marshallData[i].position = vertices[i];
                 if (normals != null) marshallData[i].normal = normals[i];
+                if (tangents != null) marshallData[i].tangent = tangents[i];
                 if (colors != null) marshallData[i].color = colors[i];
                 if (uvs  != null) marshallData[i].uv = uvs[i];
             }
@@ -213,6 +318,7 @@ namespace OpenTKBase
             // Check if shader needs a specific attribute
             if (vertices != null) SetupVAO(shader, "position", 3, VertexAttribPointerType.Float, false);
             if (normals != null) SetupVAO(shader, "normal", 3, VertexAttribPointerType.Float, false);
+            if (tangents != null) SetupVAO(shader, "tangent", 4, VertexAttribPointerType.Float, false);
             if (colors != null) SetupVAO(shader, "color", 4, VertexAttribPointerType.Float, false);
             if (uvs != null) SetupVAO(shader, "uv", 2, VertexAttribPointerType.Float, false);
 
