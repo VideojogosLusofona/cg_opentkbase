3e743851531b6af6292a3f20ea90622774c134d8
Diogo de Andrade <diogo.g.andrade@gmail.com>
2024-10-28 16:45:27 +0000

    Fixed bug with slot index

--- Shader.cs
+++ Shader.cs
@@ -332,7 +332,8 @@ namespace OpenTKBase
             for (int i = 0; i < numUniforms; i++)
             {
                 GL.GetActiveUniform(handle, i, 256, out int length, out int size, out ActiveUniformType type, out string uniformName);
-
+                int slot = GL.GetUniformLocation(handle, uniformName);
+                if (slot == -1) continue;
                 if (uniformName.StartsWith("Material"))
                 {
                     // This is a material property
@@ -340,7 +341,19 @@ namespace OpenTKBase
                     {
                         type = Uniform.Type.Material,
                         name = uniformName.Substring(8),
-                        slot = i,
+                        slot = slot,
+                        dataSize = size,
+                        dataType = type
+                    });
+                }
+                else if (uniformName.StartsWith("Env"))
+                {
+                    // This is a material property
+                    uniforms.Add(new Uniform()
+                    {
+                        type = Uniform.Type.Environment,
+                        name = uniformName.Substring(3),
+                        slot = slot,
                         dataSize = size,
                         dataType = type
                     });
@@ -353,19 +366,7 @@ namespace OpenTKBase
                         type = Uniform.Type.Matrix,
                         name = uniformName.Substring(6),
                         matrixType = StringToMatrixType(uniformName.Substring(6)),
-                        slot = i,
-                        dataSize = size,
-                        dataType = type
-                    });
-                }
-                else if (uniformName.StartsWith("Env"))
-                {
-                    // This is a material property
-                    uniforms.Add(new Uniform()
-                    {
-                        type = Uniform.Type.Environment,
-                        name = uniformName.Substring(3),
-                        slot = i,
+                        slot = slot,
                         dataSize = size,
                         dataType = type
                     });
@@ -376,7 +377,7 @@ namespace OpenTKBase
                     {
                         type = Uniform.Type.Environment,
                         name = uniformName,
-                        slot = i,
+                        slot = slot,
                         dataSize = size,
                         dataType = type
                     });
