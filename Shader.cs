e21df70010f99d05344c5ec55b93b491b18ff5db
Diogo de Andrade <diogo.g.andrade@gmail.com>
2024-10-28 16:11:20 +0000

    Fixed issue with uniform setup (slot wasn't being read)

--- Shader.cs
+++ Shader.cs
@@ -310,7 +310,7 @@ namespace OpenTKBase
             for (int i = 0; i < numUniforms; i++)
             {
                 GL.GetActiveUniform(handle, i, 256, out int length, out int size, out ActiveUniformType type, out string uniformName);
-
+                int slot = GL.GetUniformLocation(handle, uniformName);
                 if (uniformName.StartsWith("Material"))
                 {
                     // This is a material property
@@ -318,7 +318,7 @@ namespace OpenTKBase
                     {
                         type = Uniform.Type.Material,
                         name = uniformName.Substring(8),
-                        slot = i,
+                        slot = slot,
                         dataSize = size,
                         dataType = type
                     });
@@ -331,7 +331,7 @@ namespace OpenTKBase
                         type = Uniform.Type.Matrix,
                         name = uniformName.Substring(6),
                         matrixType = StringToMatrixType(uniformName.Substring(6)),
-                        slot = i,
+                        slot = slot,
                         dataSize = size,
                         dataType = type
                     });
